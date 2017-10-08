c8e774dda9d625f6a4767a251bd55108eb0f31c5
Stephen Hopkins <raidofive@gmail.com>
2017-10-07 21:01:21 -0400

    Add files via upload

---
+++ data/friends.js
@@ -0,0 +1,89 @@
+var friendArray = [
+  {
+    name: "Jon",
+    photo: "https://68.media.tumblr.com/avatar_0aeca7a262ac_128.png",
+    scores: [
+      "2",
+      "5",
+      "3",
+      "1",
+      "5",
+      "2",
+      "3",
+      "3",
+      "3",
+      "2"
+    ],
+    diff: 0
+  },
+  {
+    name: "Joe",
+    photo: "https://68.media.tumblr.com/avatar_c11820e0c39e_128.png",
+    scores: [
+      "1",
+      "5",
+      "4",
+      "1",
+      "2",
+      "3",
+      "2",
+      "2",
+      "5",
+      "5"
+    ],
+    diff: 0
+  },
+  {
+    name: "Jack",
+    photo: "https://68.media.tumblr.com/avatar_e9bf3a249960_128.png",
+    scores: [
+      "5",
+      "4",
+      "1",
+      "3",
+      "4",
+      "5",
+      "3",
+      "2",
+      "1",
+      "5"
+    ],  
+    diff: 0
+  }, 
+  {
+    name: "Jill",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr1rBm0_i9_Rcx7JpH-F7DfVmsgaxpQPhaIZ-g2Xj20hxrZr52",
+    scores: [
+      "1",
+      "5",
+      "3",
+      "5",
+      "5",
+      "4",
+      "5",
+      "2",
+      "1",
+      "1"
+    ], 
+    diff: 0 
+  },
+  {
+    name: "Jim",
+    photo: "https://pbs.twimg.com/profile_images/810859404312584193/tkY7yIQ-_reasonably_small.jpg",
+    scores: [
+      "2",
+      "5",
+      "3",
+      "2",
+      "1",
+      "3",
+      "3",
+      "5",
+      "5",
+      "5"
+    ],    
+    diff: 0
+  }   
+];
+
+module.exports = friendArray;
